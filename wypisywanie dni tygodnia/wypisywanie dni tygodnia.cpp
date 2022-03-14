57dab5b8f549c4f628ca15fccdec5df204c62c0e
user <user@JM>
2022-03-14 19:48:20 +0100

    Dodaj pliki projektów.

---
+++ wypisywanie dni tygodnia/wypisywanie dni tygodnia.cpp
@@ -0,0 +1,34 @@
+﻿
+
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+
+using namespace std;
+
+
+int main()
+{
+   
+        system("color 5");
+        cout << "poniedzialek" << endl;
+        Sleep(1000);
+        system("cls");
+        cout << "wtorek" << endl;
+        Sleep(1000);
+        system("cls");
+        cout << "sroda" << endl;
+        Sleep(1000);
+        system("cls");
+        cout << "cawartek" << endl;
+        Sleep(1000);
+        system("cls");
+        cout << "piatek" << endl;
+        Sleep(1000);
+        system("cls");
+        cout << "sobota" << endl;
+        Sleep(1000);
+        system("cls");
+        cout << "niedziela" << endl;
+        return 0;
+}
